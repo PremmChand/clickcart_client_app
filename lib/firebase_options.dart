b2dca70313d7cd866e6520eab5acaafe433f1e6b
Premm Chand <premmchand131@gmail.com>
2025-06-30 15:58:15 +0530

    firebase_option file updated

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 
 FirebaseOptions firebaseOptions = Platform.isAndroid
     ? const FirebaseOptions(
-  apiKey: 'AIzaSyAc-SWFpu7bwoz4XHQDxlcw7d83dPsT8KU',
+  apiKey: 'use your firebase api key here',
   appId: '1:375829687117:android:ab6bfbe3b58c5c96126504',
   messagingSenderId: '375829687117',
   projectId: 'clickcart-12f24',
